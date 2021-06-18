f9211856c7059a31b8cd9f6a0180d138c84d9461
crisbertal <36407578+crisbertal@users.noreply.github.com>
2021-06-18 09:53:08 +0200

    Chocolatey first check

---
+++ choco_scripts.bat
@@ -0,0 +1,13 @@
+:: Chocolatey install script from earlier
+
+@powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+:: Install all the packages
+
+choco install googlechrome -fy
+choco install vscode -fy
+choco install discord -fy
+choco install office-tool -fy
+choco install obsidian -fy
+choco install dropbox -fy
+choco install zotero -fy
